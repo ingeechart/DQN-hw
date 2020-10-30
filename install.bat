0db20161692a798525c263b471588fbf7e55ce96
rapsealk <piono623@naver.com>
2020-10-31 04:27:17 +0900

    colab: flappy bird (#1)

--- install.bat
+++ install.bat
@@ -5,7 +5,8 @@ python -m pip install -r requirements.txt
 cd gym-flappy-bird && python -m pip install -e . && cd ..
 
 :: 3. Install PyGame-Learning-Environment(ple).
-:: 3.1 git clone (PLE를 수정하기 위하여 submodule로 추가하지 않음.)
-git clone https://github.com/ntasfi/PyGame-Learning-Environment.git
+:: 3.1 git submodule
+:: git clone https://github.com/ntasfi/PyGame-Learning-Environment.git
+git submodule add https://github.com/ntasfi/PyGame-Learning-Environment.git
 :: 3.3 Install PyGame-Learning-Environment
 cd PyGame-Learning-Environment && python -m pip install -e . && cd ..
