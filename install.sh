e953ede0f5b0126dd93b4ad01d23e97a66759ed0
rapsealk <piono623@naver.com>
2020-11-01 14:40:55 +0900

    colab: Documentation

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-:: 0. Clone with submodule
+#!/usr/bin/bash
+# 0. Clone with submodule
 git clone https://github.com/ingeechart/DQN-hw.git --recurse-submodules
-::git submodule add https://github.com/ntasfi/PyGame-Learning-Environment.git
+#git submodule add https://github.com/ntasfi/PyGame-Learning-Environment.git
 
-:: 1. Install required modules.
+# 1. Install required modules.
 python -m pip install -r requirements.txt
 
-:: 2. Install gym-flappy-bird
+# 2. Install gym-flappy-bird
 cd gym-flappy-bird && python -m pip install -e . && cd ..
 
-:: 3. Install PyGame-Learning-Environment(ple).
+# 3. Install PyGame-Learning-Environment(ple).
 cd PyGame-Learning-Environment && python -m pip install -e . && cd ..
